684ba322743992aad061bd11f9e7b806ea0b6e50
mayur <shahmayur001@gmail.com>
2016-01-05 16:53:30 +0530

    file is save

---
+++ clients_controller.rb
@@ -0,0 +1,30 @@
+class ClientsController < ApplicationController
+
+
+def new
+	@client=Client.new
+end
+
+def create
+	@client=Client.new(user_param)
+	if @client.save
+		redirect_to client_path(:id=>@client.id)
+	else
+		render 'new'
+	end
+end
+
+def show
+@client=Client.find(params[:id])
+end
+
+def result 	
+	@result=Client.where("#{params["searchrd"]} like ?" ,params["searchtxt"])
+end
+
+
+	private
+	def user_param
+		params.require(:client).permit(:first_name,:last_name,:email,:password,:address,:gender,:hobby,:dob,:language)
+	end
+end
